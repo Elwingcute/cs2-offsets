7231c49b8a916b6b4b48323855a97a520ba506df
Emir can Aktas <elwing1337@proton.me>
2026-02-26 05:06:19 +0500

    02/26/2026

--- buttons.rs
+++ buttons.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 00:46:24.613794500 UTC
+// 2026-02-26 00:04:38.510709400 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
--- rendersystemdx11_dll.cs
+++ rendersystemdx11_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 00:46:24.613794500 UTC
+// 2026-02-26 00:04:38.510709400 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: rendersystemdx11.dll
